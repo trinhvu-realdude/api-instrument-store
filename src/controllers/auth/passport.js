93557c7ca2e3995daef8353386b60e41bae8697c
trinhvu-realdude <vudangtrinh29102000@gmail.com>
2022-07-21 22:15:34 +0700

    add debug log for login function

--- src/controllers/auth/passport.js
+++ src/controllers/auth/passport.js
@@ -33,7 +33,6 @@ module.exports = (passport) => {
                 return done(null, user);
 
             } catch (e) {
-                console.log(e);
                 return done(null, false);
             }
         })
